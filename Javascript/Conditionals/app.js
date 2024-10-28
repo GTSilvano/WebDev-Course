e6055fd8bc5710babde620b6ce372ad250c1da85
GTSilvano <silvano.condrea@mgail.com>
2024-10-28 11:06:35 +0200

    progress in conditionals.

---
+++ Javascript/Conditionals/app.js
@@ -0,0 +1,30 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5!");
+//     console.log(random);
+// }
+
+// if (random >= 0.5) {
+//     console.log("Your number is greater than 0.5!");
+//     console.log(random);
+// }
+
+const dayOfWeek = "Monday";
+
+if (dayOfWeek === "Monday") {
+  console.log("Ughhh, I hate Mondays!");
+} else if (dayOfWeek === "Saturday") {
+  console.log("Yay, I love Saturdays!");
+} else if (dayOfWeek === "Friday") {
+  console.log("Fridays are decent, especially after work!");
+}
+
+// const age = 2;
+
+// if (age < 5) {
+//   console.log("You're a baby, get in free!");
+// } else if (age < 10) {
+//   console.log("You're a child, pay 10$!");
+// } else if (age < 65) {
+//   console.log("You're an adult, pay 20$!");
+// }
