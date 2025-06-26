663bc4671442b18b45917107a069b4cccb514cfb
GTSilvano <silvano.condrea@mgail.com>
2025-06-26 13:10:24 +0300

    Loops, but taking a break, will continue later.

---
+++ Javascript/While_Loops/app.js
@@ -0,0 +1,29 @@
+// let count = 0;
+// while (count < 10) {
+//   count++;
+//   console.log(count);
+// }
+
+// const SECRET = "BabyHippo";
+
+// let guess = prompt("Enter the secret code...");
+// while (guess !== SECRET) {
+//   guess = prompt("Enter the secret code...");
+// }
+// console.log("Congrates you got the secret!!!");
+
+// let input = prompt("Hey, say something!");
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLocaleLowerCase() === "stop copying me") {
+//     break;
+//   }
+// }
+// console.log("Ok you win!");
+
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+  if (i === 100) {
+    break;
+  }
+}
