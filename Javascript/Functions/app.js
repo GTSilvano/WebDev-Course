c633033cd294a1d0d2cb26a98968c6ad50b9d23a
GTSilvano <silvano.condrea@mgail.com>
2025-07-05 00:53:07 +0300

    Add initial implementation of singSong and greet functions in app.js

---
+++ Javascript/Functions/app.js
@@ -0,0 +1,11 @@
+function singSong() {
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
+singSong();
+
+function greet(name) {
+  console.log(`Hi ${name}!`);
+}
+greet("Silvano");
